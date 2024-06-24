26a669e6750ad2a99ba73e1e8317dd37172b0ed9
ahmadexe <muahmad710@gmail.com>
2024-06-24 17:43:19 +0500

    fix: port fixes

--- producer/main.go
+++ producer/main.go
@@ -12,7 +12,7 @@ func main() {
 	topic := "my-topic"
 	partition := 0
 
-	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", topic, partition)
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:29092", topic, partition)
 	if err != nil {
 		log.Fatal("failed to dial leader:", err)
 	}
